b392428925a8db314560b526bee43433bcac1330
vinicius <viniciusfod@gmail.com>
2024-05-27 18:41:46 -0300

    prisma vercel

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const cors ={
-    origin: ['http://localhost:3000', process.env.FRONTEND_URL],
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL, 'https://paguei.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
   }
 
