2ecbd555880e42c5e92585dfe152ac4aeb5ca041
vinicius <viniciusfod@gmail.com>
2024-05-28 22:00:40 -0300

    prisma vercel

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const cors ={
-    origin: [process.env.FRONTEND_URL],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
-  }
+  const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
 
-  app.enableCors(cors)
-  
+  app.enableCors(corsOptions);
+
   await app.listen(3005);
 }
 bootstrap();
