a32dfb22d6e7ce5ec3ee1856d1f632f9b3e7f3ea
Vinicius <vinisaio10@gmail.com>
2025-05-28 14:34:53 -0300

    cors all

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  
+  app.enableCors({
+    origin: true, // ou você pode especificar os domínios permitidos: ['http://localhost:3000', 'https://seusite.com']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   
   await app.listen(3005);
 }
